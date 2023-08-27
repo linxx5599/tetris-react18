5061d5761b514df054f0738a5947023bd3723240
linjy <linxx5599@163.com>
2023-08-27 19:55:58 +0800

    feat: 添加背景图片

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 import Container from "@/components/Container";
 
+const bg = require("@/assets/images/bg.png").default;
 const App: React.FC = () => {
-  return <Container />
+  return (
+    <div className="warp" style={{backgroundImage: `url(${bg})` }}>
+      <Container />
+    </div>
+  );
 };
 
 export default App;
